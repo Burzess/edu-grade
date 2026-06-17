e8b7fb53d8c5ac15b9198355db5f91fe5cfdc9ce
Halim <lamanele23@gmail.com>
2026-06-17 08:32:24 +0700

    chore: add testing config and update dependencies

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src/', import.meta.url))
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: ['./tests/setup-env.ts'],
+    coverage: {
+      provider: 'v8'
+    }
+  }
+})
